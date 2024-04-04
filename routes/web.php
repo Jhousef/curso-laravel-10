8f0bc861f796438218ffde5914a370b208677543
jhousef <jhousefkl@hotmail.com>
2024-04-03 23:56:40 -0300

    parte do modulo de suportes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Site\SiteController;
+use App\Models\Support;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
+Route::get('/contato', [SiteController::class, 'contact'])->name('contato.index');
 
 Route::get('/', function () {
     return view('welcome');
