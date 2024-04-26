1d0de91d5b6188876e1813196ebadb35916f39e1
jhousef <jhousefkl@hotmail.com>
2024-04-26 19:56:04 -0300

    feat: implements store and update repository support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,26 +2,33 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
-    {
-        $supports = $support->all();
+    public function __construct(
+        protected SupportService $service
+    ){}
 
+    public function index(Request $request)
+    {
+        $supports = $this->service->getAll($request->filter);
+    dd($supports);
         return view('admin/supports/index', compact('supports'));
     }
 
-    public function show(string|int $id)
+    public function show(string $id)
     {
         // Support::find($id);
         // Support::where('id', $id)->first();
         // Support::where('id', '!=', $id)->first();
-        if(!$support = Support::find($id)){
+        if(!$support = $this->service->findOne($id)){
             return back();
         }
         return view('admin/supports/show', compact('support'));
@@ -34,17 +41,16 @@ class SupportController extends Controller
 
     public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = ($request->validated());
-        $data['status'] = 'p';
-
-        $support->create($data);
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
 
         return redirect()->route('supports.index');
     }
 
-    public function edit(Support $support, string|int $id)
+    public function edit(string $id)
     {
-        if(!$support = $support->where('id', $id)->first()){
+        if(!$support = $this->service->findOne($id)){
             return back();
         }
 
@@ -53,7 +59,11 @@ class SupportController extends Controller
 
     public function update(StoreUpdateSupport $request, Support $support, string|int $id)
     {
-        if(!$support = $support->find($id)) {
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request),
+        );
+
+        if(!$support){
             return back();
         }
 
@@ -61,18 +71,12 @@ class SupportController extends Controller
         // $support->body = $request->body;
         // $support->save();
 
-        $support->update($request->validated());
-
         return redirect()->route('supports.index');
     }
 
-    public function destroy(Support $support, string|int $id)
+    public function destroy(string $id)
     {
-        if(!$support = $support->find($id)){
-            return back();
-        }
-
-        $support->delete();
+        $this->service->delete($id);
 
         return redirect()->route('supports.index');
     }
